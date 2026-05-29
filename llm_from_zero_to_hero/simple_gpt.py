2ebc3e0ae5ff7d7d1249c9de4822e9fe89defc9f
lehoanglong95 <lehoanglong95@gmail.com>
2026-05-29 23:32:28 +1000

    prepare data for LLM

--- llm_from_zero_to_hero/simple_gpt.py
+++ llm_from_zero_to_hero/simple_gpt.py
@@ -12,6 +12,7 @@ import torch.functional as F
 
 BATCH_SIZE = 4
 CONTEXT_LENGTH = 8
+EVAL_ITERS = 300
 
 with open("./tiny_shakespeare.txt") as f:
     data = f.read()
@@ -28,7 +29,35 @@ val_data = data[threshold:]
 def get_data(split: str):
     dataset = train_data if split == "train" else val_data
     random_idxs = torch.randint(0, len(dataset) - CONTEXT_LENGTH, (BATCH_SIZE, ))
+    x_stack = []
+    y_stack = []
     for idx in random_idxs:
-        
+        x_stack.append(torch.tensor(dataset[idx: idx + CONTEXT_LENGTH], dtype=torch.long))
+        y_stack.append(torch.tensor(dataset[idx + 1: idx + 1 + CONTEXT_LENGTH], dtype=torch.long))
 
-class BigramModel()
+    return torch.stack(x_stack), torch.stack(y_stack)
+
+    
+@torch.no_grad
+def calculate_loss(model):
+    out = {}
+    for split in ["train", "val"]:
+        model.eval()
+        losses = torch.zeros(EVAL_ITERS)
+        for k in range(EVAL_ITERS):
+            X, Y = get_data(split)
+            _, loss = model(X, Y)
+            losses[k] = loss.item()
+        out[split] = losses.mean()
+    return out
+
+class BigramModel(nn.Module):
+
+    def __init__(self, vocab_size):
+        pass
+    
+    def forward(self, X, Y):
+        pass
+
+    def generate(self, X):
+        pass
